09971c846977e11c2b3ee03383341c5ea0dcbe28
jjribeiro1 <jvsr.99@hotmail.com>
2022-10-26 18:14:36 -0300

    "definição user entity e create-dto"

--- src/user/dto/create-user.dto.ts
+++ src/user/dto/create-user.dto.ts
@@ -1,1 +1,6 @@
-export class CreateUserDto {}
+export class CreateUserDto {
+  name: string;
+  username: string;
+  password: string;
+  isAdmin: boolean;
+}
--- src/user/entities/user.entity.ts
+++ src/user/entities/user.entity.ts
@@ -1,1 +1,7 @@
-export class User {}
+export class User {
+  id?: number;
+  name: string;
+  username: string;
+  password: string;
+  isAdmin: boolean;
+}
