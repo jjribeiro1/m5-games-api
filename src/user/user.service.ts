8a5d915fe73fb53a46d4c24b91fba567b739a779
jjribeiro1 <jvsr.99@hotmail.com>
2022-10-26 18:51:55 -0300

    "create user e bcrypt"

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -2,12 +2,25 @@ import { Injectable } from '@nestjs/common';
 import { PrismaService } from 'src/prisma/prisma.service';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
+import { User } from './entities/user.entity';
+import * as bcrypt from 'bcrypt';
 
 @Injectable()
 export class UserService {
   constructor(private readonly prisma: PrismaService) {}
+  private UserSelect = {
+    id: true,
+    name: true,
+    username: true,
+    password: false,
+    isAdmin: true,
+  };
   async create(dto: CreateUserDto) {
-    return this.prisma.user.create({ data: dto });
+    const data: User = {
+      ...dto,
+      password: await bcrypt.hash(dto.password, 10),
+    };
+    return this.prisma.user.create({ data, select: this.UserSelect });
   }
 
   async findAll() {
