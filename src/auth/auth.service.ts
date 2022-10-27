7cfc77c9d35d3709e988c88c06549379b53c7809
jjribeiro1 <jvsr.99@hotmail.com>
2022-10-27 16:03:25 -0300

    "verificação completa do username e password no login"

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,9 +1,29 @@
-import { Injectable } from '@nestjs/common';
+import { BadRequestException, Injectable } from '@nestjs/common';
+import { PrismaService } from 'src/prisma/prisma.service';
 import { LoginDto } from './dto/login.dto';
+import * as bcrypt from 'bcrypt';
+import { User } from 'src/user/entities/user.entity';
 
 @Injectable()
 export class AuthService {
-  login(dto: LoginDto) {
-    throw new Error('Method not implemented.');
+  constructor(private readonly prisma: PrismaService) {}
+  async login(dto: LoginDto) {
+    const { username, password } = dto;
+
+    const user: User = await this.prisma.user.findUnique({
+      where: { username },
+    });
+
+    if (!user) {
+      throw new BadRequestException('Invalid username or password');
+    }
+
+    const isPasswordValid = await bcrypt.compare(password, user.password);
+
+    if (!isPasswordValid) {
+      throw new BadRequestException('Invalid username or password');
+    }
+
+    return `success`;
   }
 }
