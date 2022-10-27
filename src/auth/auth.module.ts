5ef16ba532207afe6dacb8a4b2c700f074f0e750
jjribeiro1 <jvsr.99@hotmail.com>
2022-10-27 17:56:03 -0300

    "importação no auth module e protegendo a rota findAll users com a estrategia jwt"

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -3,16 +3,19 @@ import { PrismaModule } from 'src/prisma/prisma.module';
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
 import { JwtModule } from '@nestjs/jwt';
+import { JwtStrategy } from './strategy/jwt.strategy';
+import { PassportModule } from '@nestjs/passport';
 
 @Module({
   imports: [
     PrismaModule,
+    PassportModule,
     JwtModule.register({
       secret: process.env.JWT_SECRET,
       signOptions: { expiresIn: '24h' },
     }),
   ],
   controllers: [AuthController],
-  providers: [AuthService],
+  providers: [AuthService, JwtStrategy],
 })
 export class AuthModule {}
