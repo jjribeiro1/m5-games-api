bd5e140354d6ebb28d44ec115f653a65ad2d4ed4
jjribeiro1 <jvsr.99@hotmail.com>
2022-10-31 15:30:44 -0300

    "nest g resource genre"

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,9 +6,17 @@ import { UserModule } from './user/user.module';
 import { AuthModule } from './auth/auth.module';
 import { ProfileModule } from './profile/profile.module';
 import { GameModule } from './game/game.module';
+import { GenreModule } from './genre/genre.module';
 
 @Module({
-  imports: [PrismaModule, UserModule, AuthModule, ProfileModule, GameModule],
+  imports: [
+    PrismaModule,
+    UserModule,
+    AuthModule,
+    ProfileModule,
+    GameModule,
+    GenreModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
---
+++ src/genre/genre.controller.ts
@@ -0,0 +1,42 @@
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Patch,
+  Param,
+  Delete,
+} from '@nestjs/common';
+import { GenreService } from './genre.service';
+import { CreateGenreDto } from './dto/create-genre.dto';
+import { UpdateGenreDto } from './dto/update-genre.dto';
+
+@Controller('genre')
+export class GenreController {
+  constructor(private readonly genreService: GenreService) {}
+
+  @Post()
+  create(@Body() dto: CreateGenreDto) {
+    return this.genreService.create(dto);
+  }
+
+  @Get()
+  findAll() {
+    return this.genreService.findAll();
+  }
+
+  @Get(':id')
+  findOne(@Param('id') id: string) {
+    return this.genreService.findOne(+id);
+  }
+
+  @Patch(':id')
+  update(@Param('id') id: string, @Body() dto: UpdateGenreDto) {
+    return this.genreService.update(+id, dto);
+  }
+
+  @Delete(':id')
+  remove(@Param('id') id: string) {
+    return this.genreService.remove(+id);
+  }
+}
