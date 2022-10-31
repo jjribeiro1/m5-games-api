b7bc9fbabf0bf2bec27e308c3db6f5016b9947f2
jjribeiro1 <jvsr.99@hotmail.com>
2022-10-31 16:11:25 -0300

    "create-game dto e entity"

--- src/game/dto/create-game.dto.ts
+++ src/game/dto/create-game.dto.ts
@@ -1,1 +1,11 @@
-export class CreateGameDto {}
+export class CreateGameDto {
+  name: string;
+  image: string;
+  description: string | null;
+  year: number;
+  imdb_score: number;
+  trailer_youtube_url: string;
+  gameplay_youtube_url: string;
+
+  genres: Array<number>;
+}
--- src/game/entities/game.entity.ts
+++ src/game/entities/game.entity.ts
@@ -1,1 +1,13 @@
-export class Game {}
+import { Genre } from '../../genre/entities/genre.entity';
+
+export class Game {
+  id?: number;
+  name?: string;
+  image?: string;
+  description?: string | null;
+  year?: number;
+  imdb_score?: number;
+  trailer_youtube_url?: string;
+  gameplay_youtube_url?: string;
+  genres?: Genre[];
+}
