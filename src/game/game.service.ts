187fdf60f29019e25c413b32683bc96ec284540f
jjribeiro1 <jvsr.99@hotmail.com>
2022-10-31 16:53:58 -0300

    "implementação create game"

--- src/game/game.service.ts
+++ src/game/game.service.ts
@@ -1,11 +1,45 @@
 import { Injectable } from '@nestjs/common';
+import { PrismaService } from 'src/prisma/prisma.service';
+import { Prisma } from '@prisma/client';
 import { CreateGameDto } from './dto/create-game.dto';
 import { UpdateGameDto } from './dto/update-game.dto';
 
 @Injectable()
 export class GameService {
-  create(dto: CreateGameDto) {
-    return 'This action adds a new game';
+  constructor(private readonly prisma: PrismaService) {}
+
+  async create(dto: CreateGameDto) {
+    const data: Prisma.GameCreateInput = {
+      name: dto.name,
+      image: dto.image,
+      year: dto.year,
+      imdb_score: dto.imdb_score,
+      trailer_youtube_url: dto.trailer_youtube_url,
+      gameplay_youtube_url: dto.gameplay_youtube_url,
+      genres: {
+        createMany: {
+          data: dto.genres.map((number) => ({
+            genreId: number,
+          })),
+        },
+      },
+    };
+
+    return this.prisma.game
+      .create({
+        data,
+        select: {
+          id: true,
+          name: true,
+          year: true,
+          genres: {
+            select: {
+              genre: true,
+            },
+          },
+        },
+      })
+      .catch((err) => console.log(err.message));
   }
 
   findAll() {
