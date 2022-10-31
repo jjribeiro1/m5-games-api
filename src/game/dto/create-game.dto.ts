af1fef1db556a70fe56deba6a648a7d2ed385d05
jjribeiro1 <jvsr.99@hotmail.com>
2022-10-31 16:53:25 -0300

    "create-game dto class validator"

--- src/game/dto/create-game.dto.ts
+++ src/game/dto/create-game.dto.ts
@@ -1,11 +1,42 @@
+import {
+  IsNotEmpty,
+  IsNumber,
+  IsString,
+  IsUrl,
+  MinLength,
+  IsOptional,
+  ArrayNotEmpty,
+} from 'class-validator';
+
 export class CreateGameDto {
+  @IsString()
+  @IsNotEmpty()
+  @MinLength(3)
   name: string;
+
+  @IsUrl()
   image: string;
-  description: string | null;
+
+  @IsOptional()
+  @IsString()
+  @IsNotEmpty()
+  @MinLength(10)
+  description?: string | null;
+
+  @IsNumber()
   year: number;
+
+  @IsNumber()
   imdb_score: number;
+
+  @IsString()
+  @IsNotEmpty()
   trailer_youtube_url: string;
+
+  @IsString()
+  @IsNotEmpty()
   gameplay_youtube_url: string;
 
+  @ArrayNotEmpty()
   genres: Array<number>;
 }
